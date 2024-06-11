ee56d9a68922c5aaff66af1cddd8dd8e79f15093
Mr Hackerman <someuser@gmail.com>
2024-06-11 13:39:19 +0300

    добавлено отобаржение авторизации и меню в мобильном разрешении

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,23 +1,94 @@
-import { Button, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  HStack,
+  Heading, Image,
+  Text, useDisclosure
+} from "@chakra-ui/react";
 import { $isAuthenticated, logout } from "../models/auth";
 import { useUnit } from "effector-react";
 import logo from '../assets/logo.svg';
 import { $user } from "../models/user";
 import { LanguageSelect } from "./LanguageSelect";
 import { useTranslation } from "react-i18next";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { useIsMobile } from "../shared/hooks";
+
+interface IDrawerMenu {
+  children: React.ReactNode;
+}
+
+const DrawerMenu = ({ children }: IDrawerMenu) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  
+  return (
+    <>
+      <Button colorScheme='white' variant="ghost" onClick={onOpen}>
+        <HamburgerIcon w={6} h={6} />
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          {children}
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
 
 const Header = () => {
   const isAuthenticated = useUnit($isAuthenticated);
   const user = useUnit($user);
 
   const { t } = useTranslation();
+  const isMobile = useIsMobile();
+  
+  const appLogo = (
+    <HStack>
+      <Image src={logo} borderRadius="50%" boxSize="50px" />
+      <Heading size="md" userSelect="none">Audio Workshop</Heading>
+    </HStack>
+  );
+  
+  if (isMobile) {
+    return (
+      <HStack justify="space-between">
+        {appLogo}
+        {isAuthenticated && (
+          <DrawerMenu>
+            <DrawerHeader>
+              <Text color="black">
+                {user?.email}
+              </Text>
+            </DrawerHeader>
+
+            <DrawerBody />
+
+            <DrawerFooter justifyContent="space-between">
+                <LanguageSelect />
+                <Button size="sm" hidden={!isAuthenticated} onClick={() => logout()}>{t('log_out')}</Button>
+            </DrawerFooter>
+          </DrawerMenu>
+        )}
+      </HStack>
+    );
+  }
   
   return (
     <HStack justify="space-between">
-        <HStack>
-          <Image src={logo} backgroundColor="#f36834" borderRadius="50%" boxSize="50px" />
-          <Heading size="md" userSelect="none">Audio Workshop</Heading>
-        </HStack>
+        {appLogo}
 
         <HStack>
           <LanguageSelect />
--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -12,8 +12,8 @@ const AuthLayout = ({ headerContent }: IAuthLayout) => (
                 {headerContent}
             </div>
         </header>
-        <main className="relative flex flex-col justify-center h-screen bg-gray-50 z-0">
-            <div className="relative bg-white px-6 pt-8 pb-8 shadow-xl ring-1 w-160 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+        <main className="relative flex flex-col justify-center h-screen md:bg-gray-50 z-0">
+            <div className="relative bg-white px-6 pt-8 pb-8 md:shadow-xl md:ring-1 md:w-160 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
                 <Outlet />
             </div>
         </main>
